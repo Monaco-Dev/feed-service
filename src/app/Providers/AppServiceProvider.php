78648ab1a95beb9186978a271dc67d726b9bd0b5
Danniel Libor <32063464+blacksmithHub@users.noreply.github.com>
2024-03-09 16:58:17 +0800

    telescope

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 
 use App\Services\Contracts\PostServiceInterface;
 use App\Services\Contracts\TagServiceInterface;
@@ -36,5 +37,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
+        if (app()->environment('production')) {
+            URL::forceSchema('https');
+        }
     }
 }
